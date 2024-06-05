ee7f03c4377ed5cd2c9fd3b68fad59d0e5135322
Efe-Korumaz <9022358@student.Zadkine.nl>
2024-06-05 15:48:48 +0200

    comm

--- opdracht4.php
+++ opdracht4.php
@@ -5,33 +5,42 @@ class Product
     public $name;
     public $price;
 
+    public $category;
+
     public function setName($name)
     {
-$this->name = ucfirst($name);
+$this->name = strtolower($name);
+    }
+
+    public function setCategory($category)
+    {
+        $this->category = ucfirst($category);
     }
 
     public function formatPrice()
     {
         return number_format( $this->price, 2);
     }
-
-
+    
 }
 
 $game1 = new product;
 $game1->setName('fifa 2023');
 $game1->price = 70;
+$game1->category = 'sport';
 
 $game2 = new product();
 $game2 ->name = "hollow knight";
 $game2->price = 15;
+$game2->category = 'metro vania';
 
 
 $game3 = new product();
 $game3->name = "pokemon";
 $game3->price = 60;
+$game3->category = 'rpg';
 
 
-echo $game1-> name . $game1->formatPrice(). '<br>' ;
-echo $game2->name . '<br>';
-echo $game3->name . '<br>';
+echo $game1-> name . $game1->formatPrice(). $game1->category . '<br>' ;
+echo $game2->name . $game2->formatPrice(). $game2->category .'<br>';
+echo $game3->name. $game3->formatPrice(). $game3->category . '<br>';
